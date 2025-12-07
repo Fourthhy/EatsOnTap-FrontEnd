a80a5228b1324be2585b3649affdcfff7b19d98e
Fourthhy <miguelmanabo4@gmail.com>
2025-12-07 16:19:08 +0800

    added template for meal claim records

--- src/pages/admin/Records.jsx
+++ src/pages/admin/Records.jsx
@@ -1,7 +1,25 @@
+import { HeaderBar } from "./components/HeaderBar"
+import { MealClaimRecords } from "./components/mealClaimRecords/MealClaimRecords";
+
 export default function Records() {
+    const USER_AVATAR = "https://randomuser.me/api/portraits/lego/3.jpg";
+
+
     return (
         <>
-            this is records page
+            <div
+                style={{
+                    backgroundColor: "#F7F9F9",
+                    marginBottom: "30px",
+                }}
+                className="w-full h-auto flex flex-col justify-start">
+                <HeaderBar headerTitle="Records" userAvatar={USER_AVATAR} />
+                <div className="w-full flex justify-center">
+                    <div className="w-[95%]">
+                        <MealClaimRecords />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
