067c09cf0a52d20d6e3751c952b25a0fa5ad4733
Fourthhy <miguelmanabo4@gmail.com>
2025-12-28 02:10:00 +0800

    modified data manifest for splash loading screen

--- src/config/dataManifest.js
+++ src/config/dataManifest.js
@@ -2,40 +2,55 @@ export const APP_INITIALIZATION_MANIFEST = {
     // BATCH 1: CRITICAL (Runs first, required for Dashboard)
     // Total Weight: 40%
     critical: [
-        { 
-            id: 'auth_check', 
-            label: 'Verifying Admin Credentials...', 
-            weight: 10 
+        {
+            id: 'dash_chart_daily',
+            label: 'Fetching Daily Data for all charts (Last 6 days)...',
+            weight: 10
         },
-        { 
-            id: 'dash_daily', 
-            label: 'Aggregating Daily Meal Stats (Last 6 Days)...', 
-            weight: 15 
+        {
+            id: 'dash_chart_weekly',
+            label: 'Fetching Weekly Data for all charts (Last 4 weeks)...',
+            weight: 10
         },
-        { 
-            id: 'dash_weekly', 
-            label: 'Computing Weekly Trends (Last 4 Weeks)...', 
-            weight: 15 
-        }
+        {
+            id: 'dash_chart_monthly',
+            label: 'Fetching Monthly Data for all charts (Last 4 months)...',
+            weight: 10
+        },
+        {
+            id: 'dash_chart_overall',
+            label: 'Fetching Overall Data for all charts...',
+            weight: 10
+        },
     ],
 
     // BATCH 2: HEAVY LISTS (Runs in background after critical is done)
     // Total Weight: 60%
     secondary: [
-        { 
-            id: 'master_students', 
-            label: 'Loading Student Registry...', 
-            weight: 30 
+        {
+            id: 'master_students',
+            label: 'Loading Student Registry...',
+            weight: 15
         },
-        { 
-            id: 'master_events', 
-            label: 'Syncing Event Calendar...', 
-            weight: 20 
+        {
+            id: 'master_events',
+            label: 'Syncing Event Calendar...',
+            weight: 15
         },
-        { 
-            id: 'sys_menu', 
-            label: 'Checking Menu Availability...', 
-            weight: 10 
-        }
+        {
+            id: 'master_orders',
+            label: 'Loading Order Records...',
+            weight: 15
+        },
+        {
+            id: 'master_claim records',
+            label: 'Loading Claim Records',
+            weight: 10
+        },
+        {
+            id: 'master_claim records',
+            label: 'Opening...',
+            weight: 5
+        },
     ]
 };
