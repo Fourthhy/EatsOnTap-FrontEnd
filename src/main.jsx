a7fce9c35bbae82aa97f676a9929e2e1cd900e38
Fourthhy <miguelmanabo4@gmail.com>
2026-01-01 03:37:41 +0800

    added context on main, for usability of the state variables

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Routers from './router/Routers'
 
-
+import { DataProvider } from './context/DataContext'
+import { LoaderProvider } from './context/LoaderContext'
 
 createRoot(document.getElementById('root')).render(
-    <Routers />
+    <DataProvider>
+        <LoaderProvider>
+            <Routers />
+        </LoaderProvider>
+    </DataProvider>
 )
