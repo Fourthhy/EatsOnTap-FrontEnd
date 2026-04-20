1529e864fcbf925ce63fa305c2fefdaee3205a91
Fourthhy <miguelmanabo4@gmail.com>
2026-04-20 14:29:16 +0800

    Revert state

--- src/functions/admin/addStudentUsingCSV.js
+++ src/functions/admin/addStudentUsingCSV.js
@@ -1,7 +1,7 @@
 const VITE_LOCALHOST = import.meta.env.VITE_LOCALHOST;
 const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
 
-const BASE_URL = VITE_LOCALHOST;
+const BASE_URL = VITE_BASE_URL;
 
 export async function uploadStudentCSV(file) {
     // ⚠️ CHECK ROUTE: Matches /api/students/usingCSV
