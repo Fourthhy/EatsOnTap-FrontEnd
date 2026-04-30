01e47f4463027d39cbf6f7189b967f0d80875dc7
Fourthhy <miguelmanabo4@gmail.com>
2026-04-30 08:58:10 +0800

    Added BASE_URL for interchanging end points

--- src/functions/classAdviser/SubmitStudentMealList.js
+++ src/functions/classAdviser/SubmitStudentMealList.js
@@ -1,12 +1,14 @@
 const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
 const VITE_LOCALHOST = import.meta.env.VITE_LOCALHOST;
 
+const BASE_URL = VITE_BASE_URL
+
 // 🟢 UPDATE: Added 'forAbsent' parameter
-export async function SubmitStudentMealList(userID, section, forEligible, forAbsent) {
+export async function submitStudentMealList(userID, section, forEligible, forAbsent) {
     // do not forget to change to VITE_BASE_URL when deploying
     const token = localStorage.getItem('authToken');
 
-    const response = await fetch(`${VITE_BASE_URL}/api/eligibility/submitListforBasicEduc`, {
+    const response = await fetch(`${BASE_URL}/api/eligibility/submitListforBasicEduc`, {
         method: 'POST',
         headers: {
             'Authorization': `Bearer ${token}`,
